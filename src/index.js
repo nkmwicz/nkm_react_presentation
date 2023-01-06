3ffbdca5aadb83f69a94387c96dda41870e0c79c
Nathan Michalewicz <nathan@mwicz.com>
2023-01-06 18:51:51 -0500

    added Arrows

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Intro } from './Intro';
 export { default as SplitSlide } from './SplitSlide';
 export { default as CenteredSlide } from './CenteredSlide';
 export { default as SplitSlideChild } from './SplitSlideChild';
+export { default as Arrows } from './Arrows';
