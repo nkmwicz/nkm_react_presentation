6fe22cf58d5699e6646ec0fe196cfa8a1a30721a
Nathan Michalewicz <nathan@mwicz.com>
2023-01-06 15:02:54 -0500

    intro added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 export function Intro() {
-
+  return (
+    <>
+      <div className="bg-toulon" />
+      <div className="title">
+        <div className="title-box">
+          <h1 className="text-center">
+            France's Greatest Ally: <br />
+            The Ottoman Empire in French Foreign Policy during the Sixteenth Century</h1><br />
+          <h2 className="text-center">
+            Nathan Michalewicz
+          </h2>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export function SplitSlide() {
-
+  return (
+    null
+  )
 };
 
 export function CenteredSlide() {
-
+  return (
+    null
+  )
 };
 
 export function SplitSlideMap() {
-
+  return (
+    null
+  )
 };
