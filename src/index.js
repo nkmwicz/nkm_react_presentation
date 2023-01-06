31c6fac6d74aa03b2073d7cd45aacd6f8fc0b852
Nathan Michalewicz <nathan@mwicz.com>
2023-01-06 16:17:44 -0500

    added docs and class props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
+import "./styling.css";
 /**
- * Intro creates a basic introductory page,
+ * --Intro creates a basic introductory page,
  * taking in a title, subTitle, and byLine for 
  * metaData on the slideShow.
+ * --bgImage inputs an image inline into the component as the background.
+ * --bgColor inputs a background color onto the component with inline styling.
+ * --backgroundClass can be used to create a customized css for the component.
+ * --If using backgroundClass, bgImage is unneccessaary since
+ * a background image can be added directly to the css. The same applies to bgColor.
+ * --titleClass creates a custom class for the container holding
+ * the titleBox, the title, and the byline.
+ * --titleBoxClass can be added to customize css 
+ * on the titlebox (the container holding the title and byline)
  */
 export function Intro({
   title,
   subTitle,
-  byLine
+  byLine,
+  bgImage,
+  titleBoxClass,
+  backgroundClass,
+  titleClass,
 }) {
   return (
     <>
-      <div className="bg-intro" />
-      <div className="title">
-        <div className="title-box">
+      <div
+        className={backgroundClass ? backgroundClass : "bg-intro"}
+        style={bgImage ? {
+          backgroundImage: `url(${bgImage})`
+        } :
+          bgColor ? {
+            backgroundColor: bgColor,
+          } : null} />
+      <div className={titleClass ? titleClass : "title"}>
+        <div className={titleBoxClass ? titleBoxClass : "title-box"}>
           <h1 className="text-center">
             {title}
             <br />
