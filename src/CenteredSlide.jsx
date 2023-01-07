3a3f03e581df90c70159e7eb2cc00bb0d2dd8733
Nathan Michalewicz <nathan@mwicz.com>
2023-01-06 23:49:34 -0500

    adjusted docs

--- src/CenteredSlide.jsx
+++ src/CenteredSlide.jsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 /**
  * --headerClass is a class
  * --recommended data model for images: {image: 'string', flex: Number, description: 'String' }. image is the image url. Flex is the flex number for the image. Default value is 1. Change to 2 if you want that image to be twice the width of the other images. Description is the figcaption value and alt-text.
- * --If you prefer a series of columns of text, use textArray, which takes an array of objects: [{text: [String, [{text: 'String', color: 'String'}]], color: 'String'}].
+ * --If you prefer a series of columns of text, use textArray, which takes an array of objects: [{text: [{text: 'String', color: 'String'}, [{text: 'String', color: 'String'}]], color: 'String'}].
  * The convoluted array of arrays for text permits subitems to be incorporated into the 
  * --headerClass
  * --contentBoxClass
@@ -40,19 +40,37 @@ function CenteredSlide({
               </figure>
             ))}</div> :
           <div className={`${contentTextClass} content-text`}>{textArray.map(a => {
+            if (a.length > 0) {
+              return (
+                <>
+                  <div key={uuidv4()} className="text">
+                    <p
+                      key={uuidv4()}
+                      style={{ color: a[0].color }}>
+                      {a[0].text}
+                    </p>
+                    {a.text.length > 1 && <ul>
+                      {a[1].map(b => {
+                        return (
+                          <li key={uuidv4()} style={{ color: b.color }}>{b.text}</li>
+                        )
+                      })}
+                    </ul>}
+                  </div>
+                </>
+              )
+            }
+            // if (a.length === 0) {
             return (
-              <><div key={uuidv4()} className="text">
-                <p key={uuidv4()} style={{ color: a[0].color }}>{a[0].text}</p>
-                <ul>
-                  {a[1].map(b => {
-                    return (
-                      <li key={uuidv4()} style={{ color: b.color }}>{b.text}</li>
-                    )
-                  })}
-                </ul>
+              <div key={uuidv4()} className="text">
+                <p
+                  key={uuidv4()}
+                  style={{ color: a.color }}>
+                  {a.text}
+                </p>
               </div>
-              </>
             )
+            // }
           })}</div>}
       </div>
     </>
