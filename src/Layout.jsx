2730375b97a69ebb6c8bd138ec099c023f30e270
Nathan Michalewicz <nathan@mwicz.com>
2023-01-07 17:16:34 -0500

    initial commit

---
+++ src/Layout.jsx
@@ -0,0 +1,28 @@
+import Intro from './Intro';
+import SplitSlide from './SplitSlide';
+import Arrows from './Arrows';
+import CenteredSlide from './CenteredSlide';
+import SplitSlideChild from './SplitSlideChild';
+
+/**
+ * Layout takes a state array that follows the data model
+ * discussed for each. 
+ */
+function Layout({
+  slideArray,
+  currentSlide,
+  index,
+  setIndex,
+  ImagesToCache,
+}) {
+
+
+  return (
+    <>
+      <Arrows />
+      { }
+    </>
+  )
+};
+
+export default Layout;
