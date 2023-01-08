8f554b369c2909af03e87432c5136349d10dabae
Nathan Michalewicz <nathan@mwicz.com>
2023-01-08 13:54:56 -0500

    exports cacheImage and SplitSlideEmbed

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ export { default as SplitSlide } from './SplitSlide';
 export { default as CenteredSlide } from './CenteredSlide';
 export { default as SplitSlideChild } from './SplitSlideChild';
 export { default as Arrows } from './Arrows';
+export { default as cacheImages } from './cacheImages';
+export { default as SplitSlideEmbed } from './SplitSlideEmbed';
