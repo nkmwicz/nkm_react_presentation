0374cf0909a90611073f8d99b9b356ad8c237fb9
Nathan Michalewicz <nathan@mwicz.com>
2023-01-09 21:36:51 -0500

    added Layout

--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ export { default as cacheImages } from './cacheImages';
 export { default as SplitSlideEmbed } from './SplitSlideEmbed';
 export { default as changeSlideWithKeys } from './changeSlideWithKeys';
 export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as Layout } from './Layout';
