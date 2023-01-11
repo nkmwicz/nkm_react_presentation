2ff239557192a3a70a7d8bf86857e0d8b32da2a1
Nathan Michalewicz <nathan@mwicz.com>
2023-01-10 19:21:38 -0500

    deleted unused code

--- src/Arrows.jsx
+++ src/Arrows.jsx
@@ -25,26 +25,6 @@ function Arrows({
   handleNextClick,
   handlePrevClick,
 }) {
-  // useEffect(() => {
-  //   const click = (e) => {
-  //     if (e.keyCode === 37) {
-  //       e.preventDefault()
-  //       // left arrow
-  //       handlePrevClick
-  //       console.log(e.keyCode)
-  //     }
-  //     if (e.keyCode === 39) {
-  //       e.preventDefault()
-  //       //right arrow
-  //       handleNextClick
-  //       console.log(handleNextClick)
-  //     }
-  //   }
-  //   window.addEventListener('keyup', click, false);
-  //   return function cleanup() {
-  //     window.removeEventListener('keyup', click, false);
-  //   }
-  // });
 
   return (
     <>
