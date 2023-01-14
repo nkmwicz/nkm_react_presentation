3043b494b8a6eb8b214d57a5bc31a260d3f773ed
Nathan Michalewicz <nathan@mwicz.com>
2023-01-14 18:20:12 -0500

    exporting slider

--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ export { default as SplitSlideEmbed } from './SplitSlideEmbed';
 export { default as changeSlideWithKeys } from './changeSlideWithKeys';
 export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as QuickLayout } from './QuickLayout';
+export { default as Slider } from './Slider';
